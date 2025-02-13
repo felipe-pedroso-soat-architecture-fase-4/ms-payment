4f77075ab15b144c871f004bc7fbb6d61dad0b69
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2025-02-12 21:03:21 -0300

    test

--- main_api.ts
+++ main_api.ts
@@ -65,5 +65,5 @@ const orderGateway = new OrderGatewayHttp(new AxiosAdapter());
 const createPayment = new CreatePayment(paymentRepository, orderGateway); 
 const getPaymentStatus = new GetPaymentStatus(paymentRepository, orderGateway);
 new PaymentController(httpServer, createPayment, getPaymentStatus); 
-
+console.log("miannnnn")
 httpServer.listen(port);
